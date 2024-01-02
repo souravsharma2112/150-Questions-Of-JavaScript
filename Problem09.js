63e856927ae6f886f9e76c328146a04a36241fdc
Sourav Kumar <souravsharma2102@gmail.com>
2024-01-02 14:00:12 +0530

    update second

---
+++ Problem09.js
@@ -0,0 +1,1 @@
+// 9. Write a JavaScript program to combine the numbers of a given array into an array containing all combinations.
--- Problem10.js
+++ Problem10.js
@@ -1,1 +1,24 @@
 // 10. Write a JavaScript program to extract values at specified indexes from a specified array.
+
+// let arra1 = ['a', 'b', 'c', 'd', 'e', 'f'];
+// console.log(pull_at_Index(arra1, [1, 3]));
+// let arra2 =  [1, 2, 3, 4, 5, 6, 7];
+// console.log(pull_at_Index(arra2, [4]));
+
+// Output : 
+
+// ["b","d"]
+// [5]
+
+let arr1 = ['a', 'b', 'c', 'd', 'e', 'f'];
+let pull_at_Index = (arr, index) => {
+    let arr2 = [1];
+    for (const key in arr) {
+        if (index === key) {
+            arr2.push("2")
+        }
+    }
+    console.log(arr2);
+}
+
+pull_at_Index(arr1, 3)
